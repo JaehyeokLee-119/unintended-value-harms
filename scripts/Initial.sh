d1b1b0690e37c1e6a67803eae63748f10a3cd468
JaehyeokLee-119 <hjl8708@skku.edu>
2025-04-27 19:19:46 +0000

    path correction (preprocssing)

--- scripts/Initial.sh
+++ scripts/Initial.sh
@@ -1,6 +1,6 @@
 
-python preprocessing.py \
+python src/preprocessing.py \
     --threshold 3 \
-    --distribution_fname ../data/extreme_distributions.csv \
-    --valueEval_fname ../data/valueEval_10.csv \
-    --output_dir ../data/values
+    --distribution_fname data/extreme_distributions.csv \
+    --valueEval_fname data/valueEval_10.csv \
+    --output_dir data/values
